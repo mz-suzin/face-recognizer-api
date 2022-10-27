cd9c0b40c51cea417de3408057176edf38b5bd88
mz-suzin <mzsuzin@gmail.com>
2022-10-27 14:46:57 -0300

    fix

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const app = express();
 const db = knex({
     client: 'pg',
     connection: {
-      host : process.env.DATABASE_URL,
+      connectionString : process.env.DATABASE_URL,
       ssl: true
     }
   });
@@ -40,7 +40,7 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageUrl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen((process.env.PORT || 3000), () => {
     console.log('app is running on port ${process.env.PORT}');
 });
 
